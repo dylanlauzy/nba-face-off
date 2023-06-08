a139f62c31ff7ebbcf3afaa920be7ea41394f4f6
dylanlauzy <dylzylau@gmail.com>
2023-06-07 23:57:18 -0700

    implemented generateCards mutation and handler

--- server/server.js
+++ server/server.js
@@ -3,86 +3,85 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
 import { readFileSync } from 'fs';
 import { DynamoDBClient, GetItemCommand, ScanCommand,  BatchGetItemCommand, PutItemCommand, UpdateItemCommand  } from '@aws-sdk/client-dynamodb';
-import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { marshall, unmarshall, convertToAttr, convertToNative } from '@aws-sdk/util-dynamodb';
 import { v4 as uuidv4 } from 'uuid';
 
 const typeDefs = readFileSync(new URL('./schema.graphql', import.meta.url)).toString('utf-8');
 const dynamoDB = new DynamoDBClient({ region: "us-east-1" });
 
+const getRandomPlayers = async (count) => {
+  const params = {
+    TableName: "NBAPlayerData",
+    ProjectionExpression: "PLAYER_ID",
+    FilterExpression: "GP >:value",
+    ExpressionAttributeValues: {
+      ':value': { N: '30'}
+    }
+  }
+
+  let players;
+
+  try {
+    const command = new ScanCommand(params);
+    players = await dynamoDB.send(command);
+  } catch (error) {
+    console.error("DynamoDB error:", error);
+    throw new Error("Failed to get players from DynamoDB");
+  }
+
+  let randomIndexes = new Set();
+  let chosenPlayers = [];
+  while(randomIndexes.size < count) {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * players.Items.length);
+    } while (randomIndexes.has(newIndex))
+    randomIndexes.add(newIndex);
+    chosenPlayers.push(players.Items[newIndex])
+  }
+
+  
+  const getPlayerParams = {
+    RequestItems: {
+      NBAPlayerData: {
+        Keys: chosenPlayers.map(player => ({
+          PLAYER_ID: { N: player.PLAYER_ID.N}
+        }))
+      }
+    }
+  }
+  
+  let playerData;
+  try {
+    const command = new BatchGetItemCommand(getPlayerParams);
+    const response = await dynamoDB.send(command);
+
+    playerData = response.Responses.NBAPlayerData.map((player) => ({
+      id: player.PLAYER_ID.N,
+      name: player.PLAYER_NAME.S,
+      age: player.AGE.N,
+      team: player.TEAM_ABBREVIATION.S,
+      games: player.GP.N ,
+      pts: player.PTS.N ,
+      reb: player.REB.N ,
+      ast: player.AST.N ,
+      stl: player.STL.N ,
+      blk: player.BLK.N ,
+      fgPct: player.FG_PCT.N ,
+      ftPct: player.FT_PCT.N ,
+      fg3Pct: player.FG3_PCT.N ,
+    }))
+  } catch (error) {
+    console.error("DynamoDB error:", error);
+    return null;
+  }
+
+  return playerData;
+}
+
 const resolvers = {
   Query: {
-    getRandomPlayers: async (_, { count }) => {
-      const params = {
-        TableName: "NBAPlayerData",
-        ProjectionExpression: "PLAYER_ID",
-        FilterExpression: "GP >:value",
-        ExpressionAttributeValues: {
-          ':value': { N: '30'}
-        }
-      }
-    
-      let players;
-    
-      try {
-        const command = new ScanCommand(params);
-        players = await dynamoDB.send(command);
-      } catch (error) {
-        console.error("DynamoDB error:", error);
-        throw new Error("Failed to get players from DynamoDB");
-      }
-    
-      let randomIndexes = new Set();
-      let chosenPlayers = [];
-      while(randomIndexes.size < count) {
-        let newIndex;
-        do {
-          newIndex = Math.floor(Math.random() * players.Items.length);
-        } while (randomIndexes.has(newIndex))
-        randomIndexes.add(newIndex);
-        chosenPlayers.push(players.Items[newIndex])
-      }
-
-      
-      const getPlayerParams = {
-        RequestItems: {
-          NBAPlayerData: {
-            Keys: chosenPlayers.map(player => ({
-              PLAYER_ID: { N: player.PLAYER_ID.N}
-            }))
-          }
-        }
-      }
-      
-      let playerData;
-      try {
-        const command = new BatchGetItemCommand(getPlayerParams);
-        const response = await dynamoDB.send(command);
-
-        console.log(response);
-
-        playerData = response.Responses.NBAPlayerData.map((player) => ({
-          id: player.PLAYER_ID.N,
-          name: player.PLAYER_NAME.S,
-          age: player.AGE.N,
-          team: player.TEAM_ABBREVIATION.S,
-          games: player.GP.N ,
-          pts: player.PTS.N ,
-          reb: player.REB.N ,
-          ast: player.AST.N ,
-          stl: player.STL.N ,
-          blk: player.BLK.N ,
-          fgPct: player.FG_PCT.N ,
-          ftPct: player.FT_PCT.N ,
-          fg3Pct: player.FG3_PCT.N ,
-        }))
-      } catch (error) {
-        console.error("DynamoDB error:", error);
-        return null;
-      }
-  
-    
-      return playerData;
-    },
+    getRandomPlayers: async (_, { count }) => getRandomPlayers(count),
   },
 
   Mutation: {
@@ -131,8 +130,6 @@ const resolvers = {
       try {
         const command = new UpdateItemCommand(updateParams);
         const response = await dynamoDB.send(command);
-        console.log(response.Attributes.players.L[0]);
-        console.log(unmarshall(response.Attributes));
         return unmarshall(response.Attributes);
       } catch (error) {
         console.error(error);
@@ -141,13 +138,13 @@ const resolvers = {
     },
 
     removeFromGame: async(_, { gameId, playerId }) => {
-      const getParams = {
-        TableName: "NBA-face-off-games",
-        Key: { id: { S: gameId } },
-        ProjectionExpression: "players",
-      }
-
       try {
+        const getParams = {
+          TableName: "NBA-face-off-games",
+          Key: { id: { S: gameId } },
+          ProjectionExpression: "players",
+        }
+
         const getCommand = new GetItemCommand(getParams);
         const response = await dynamoDB.send(getCommand);
         const updatedPlayers = response.Item.players.L.filter(player => player.M.id.S != playerId);
@@ -156,8 +153,60 @@ const resolvers = {
           Key: { id: { S: gameId }},
           TableName: "NBA-face-off-games",
           UpdateExpression: "SET players = :updated",
-          ExpressionAttributeValues: {
+          ExpressionAttributeNames: {
             ":updated": { L: updatedPlayers },
+          },
+          ReturnValues: "ALL_NEW",
+        }
+
+        const updateCommand = new UpdateItemCommand(updateParams);
+        const updateResponse = await dynamoDB.send(updateCommand);
+        return unmarshall(updateResponse.Attributes);
+      } catch(error) {
+        console.error(error);
+        return null;
+      }
+    },
+
+    generateCards: async (_, { gameId }) => {
+      try {
+        const getParams = {
+          TableName: "NBA-face-off-games",
+          Key: { id: { S: gameId } },
+          ProjectionExpression: "#S, players",
+          ExpressionAttributeNames: {
+            "#S": "status"
+          }
+        }
+
+        const getCommand = new GetItemCommand(getParams);
+        const response = await dynamoDB.send(getCommand);
+
+        const status = response.Item.status.S;
+        if(status != "Waiting") throw new Error("Waiting for players");
+        
+        let players = response.Item.players.L;
+        if(players.length != 2) throw new Error("Not enough players");
+
+        players[0].M.cards.L = [];
+        players[1].M.cards.L = [];
+
+        const cards = await getRandomPlayers(30);
+
+        for(let i = 0; i < cards.length; i++) {
+          if(i % 2 == 0) {
+            players[0].M.cards.L.push(convertToAttr(cards[i]));
+          } else {
+            players[1].M.cards.L.push(convertToAttr(cards[i]));
+          }
+        }
+        
+        const updateParams = {
+          Key: { id: { S: gameId }},
+          TableName: "NBA-face-off-games",
+          UpdateExpression: "SET players = :updated",
+          ExpressionAttributeValues: {
+            ":updated": { L: players },
           },
           ReturnValues: "ALL_NEW",
         }
